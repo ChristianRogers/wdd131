939a363fb867851c968c579b19c768bca92045fc
christianrogers <mariomanrogers@gmail.com>
2025-09-17 12:29:37 -0600

    javascript for ponder part 1

---
+++ ponder/meta-css.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = PI * radius * radius;
+console.log(area);
+
+radius = 20;
+
+area = PI * radius * radius;
+console.log(area);
+
+
+                    
+                    
