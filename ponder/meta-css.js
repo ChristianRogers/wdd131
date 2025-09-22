b6ed635f878390499ad078d9df0fb4c78e115581
christianrogers <mariomanrogers@gmail.com>
2025-09-22 11:53:19 -0600

    FInished JS Ponder

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -9,6 +9,22 @@ radius = 20;
 area = PI * radius * radius;
 console.log(area);
 
+const one = 1; // integer
+const two = '2'; // string
 
-                    
+let result = one + two;
+console.log(result); // "12" (string, due to type coercion)
+
+result = one + Number(two);
+console.log(result); // 3
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+// console.log(student); //does not work, can't access a block variable outside the block
                     
